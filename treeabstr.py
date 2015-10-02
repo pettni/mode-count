4b1e871aac25140a63a18237f9236189a19b6b93
pettni <pettni@umich.edu>
2015-10-02 19:09:39 -0400

    switched to networkx graphs

--- treeabstr.py
+++ treeabstr.py
@@ -3,10 +3,24 @@ import numpy as np
 from numpy.linalg import norm
 from scipy.linalg import expm
 import scipy.integrate as integrate
+import random
+
+import networkx as nx
 
 import matplotlib.pyplot as plt
 
 import itertools
+
+def draw_modes(G):
+	maxmode = max([d['mode'] for (u,v,d) in  G.edges(data=True)]) + 1
+	edgelists = [ [(u,v) for (u,v,d) in G.edges(data=True) if d['mode'] == mode ]  for mode in range(maxmode)]
+	colors = iter(plt.cm.rainbow(np.linspace(0,1,len(edgelists) + 1)))
+
+	pos = nx.spring_layout(G)
+	nx.draw_networkx_nodes(G, pos, node_color = [next(colors)] * G.number_of_nodes() )
+	for i, edgelist in enumerate(edgelists):
+		nx.draw_networkx_edges(G, pos, edgelist = edgelist, edge_color = [next(colors)] * len(edgelist))
+
 
 def midx_to_idx(midx, mN):
 	# Given a len(mN)-dimensional matrix of with sides mN stored as a list,
@@ -29,59 +43,70 @@ def idx_to_midx(idx, mN):
 		idx = np.floor(idx / mN[i])
 	return tuple(midx)
 
-class Cell(object):
-
-	def __str__(self):
-		return str(self.lb) + " <= x <= " + str(self.ub)
-
-	def __init__(self, lb, ub):
-		self.lb = lb;
-		self.ub = ub;
-		self.edgeOut = []
-
-	def mid(self):
-		return (self.lb + self.ub) / 2
-
 class Abstraction(object):
+	""" 
+		Abstraction built on top of a networkx DiGraph
+	"""
 	def __init__(self, lb, ub, eta):
+		self.eta = eta
+
 		self.lb = np.array(lb, dtype=np.float64)
 		self.lb -= (self.lb + eta/2) % eta  # make sure that zero is in middle of cell
 		self.ub = np.array(ub, dtype=np.float64)
 		self.ub += (self.ub - self.lb) % eta # make domain number of eta's
+		self.nextMode = 0
 
-		self.n_dim = np.ceil((self.ub- self.lb)/eta).astype(np.uint64)
-		self.cell_list = [None] * np.prod(self.n_dim)
-		self.eta = eta
+		# 
+		self.n_dim = np.round((self.ub - self.lb)/eta).astype(np.uint64)
 
-		for idx in range(len(self.cell_list)):
-			midx = np.array(idx_to_midx(idx, self.n_dim))
-			cell_lb = self.lb + midx * eta
+		# Create a graph and populate it with nodes
+		self.graph = nx.DiGraph()
+		for midx in itertools.product(*[range(dim) for dim in self.n_dim]):
+			cell_lb = self.lb + np.array(midx) * eta
 			cell_ub = cell_lb + eta
-			self.cell_list[idx] = Cell(cell_lb, cell_ub)
-
-	def get_cell(self, midx):
-		assert(np.all(midx < self.n_dim))
-		return self.cell_list[midx_to_idx(midx, self.n_dim)]
+			self.graph.add_node(midx, lb = cell_lb, ub = cell_ub, mid = (cell_lb + cell_ub) / 2)
 
 	def get_midx_pt(self, pt):
+		""" 
+			Return the discret multiindex corresponding to a given continuous point
+		"""
 		assert(len(pt) == len(self.n_dim))
-		if np.any(self.lb > pt) or np.any(self.ub < pt):
+		if not self.contains(pt):
 			raise('Point outside domain')
 		midx = np.floor((np.array(pt) - self.lb) / self.eta).astype(np.uint64)
-		return midx
+		return tuple(midx)
 
-	def plot_2d(self):
+	def contains(self, pt):
+		if np.any(self.lb >= pt) or np.any(self.ub <= pt):
+			return False
+		return True
+
+	def plot_planar(self):
+		"""
+			In 2d case, plot abstraction with transitions
+		"""
 		assert(len(self.lb) == 2)
 		ax = plt.axes()
-		for cell in self.cell_list:
-			mid_this = cell.mid()
-			plt.plot(mid_this[0], mid_this[1], 'ro')
-			for eo in cell.edgeOut:
-				mid_next = self.cell_list[midx_to_idx(eo, self.n_dim)].mid()
-				ax.arrow(mid_this[0], mid_this[1],  mid_next[0] - mid_this[0], mid_next[1] - mid_this[1])
+		for node, attr in self.graph.nodes_iter(data = True):
+			plt.plot(attr['mid'][0], attr['mid'][1], 'ro')
+		for n1, n2, mode in self.graph.edges_iter(data='mode'):
+			mid1 = self.graph.node[n1]['mid']
+			mid2 = self.graph.node[n2]['mid']
+			col = 'b' if mode == 1 else 'g'
+			ax.arrow(mid1[0], mid1[1],  mid2[0] - mid1[0], mid2[1] - mid1[1], fc=col, ec=col)
 
 		plt.show()
 
+	def add_mode(self, vf, tau):
+		dummy_vf = lambda z,t : vf(z)
+		for node, attr in self.graph.nodes_iter(data=True):
+			x_fin = integrate.odeint(dummy_vf, attr['mid'], np.arange(0, tau, tau/100))
+			if self.contains(x_fin[-1]):
+				midx = self.get_midx_pt(x_fin[-1])
+				self.graph.add_edge( node, midx, mode = self.nextMode )
+			else:
+				print "Warning: there are transitions out of domain"
+		self.nextMode += 1
 
 def verify_bisim(data):
 	for beta in data['beta']: 
@@ -105,20 +130,16 @@ def plot_treeineq(data):
 	plt.show()
 
 def tree_abstr(data):
+	""" 
+		Compute an abstraction
+	"""
 	ab = Abstraction(data['lb'], data['ub'], data['eta'])
 
 	for vf in data['vf']:
-		dummy_vf = lambda z,t : vf(z)
-		for cell in ab.cell_list:
-			x_fin = integrate.odeint(dummy_vf, cell.mid(), np.arange(0, data['tau'], data['tau']/100))
-			try:
-				cell.edgeOut.append( ab.get_midx_pt(x_fin[-1]) )
-			except Exception, e:
-				pass
-
+		ab.add_mode(vf, data['tau'])
 	return ab
 
-def main():
+def example_abstr():
 	data = {}
 	# Define a vector fields
 	data['vf'] = [lambda x : [-(x[0]-1) + x[1], -(x[0]-1) - x[1]], 
@@ -138,11 +159,16 @@ def main():
 	data['tau'] = 1.1
 
 	verify_bisim(data)
-	# plot_treeineq(data)
 	verify_tree(data)
 
 	ab = tree_abstr(data) 
-	# ab.plot_2d()
+
+	return ab
+
+def main():
+	ab = example_abstr()
+	# plot_treeineq(data)
+	# ab.plot_planar()	
 
 if __name__ == '__main__':
 	main()
