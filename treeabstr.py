84691f3bac39dcd6bef66890f18bcf5d399aff8a
pettni <pettni@umich.edu>
2015-10-02 20:29:11 -0400

    can now compute matrices for linear systems

--- treeabstr.py
+++ treeabstr.py
@@ -2,7 +2,8 @@ import numpy as np
 
 from numpy.linalg import norm
 from scipy.linalg import expm
-import scipy.integrate as integrate
+import scipy.integrate
+import scipy.sparse
 import random
 
 import networkx as nx
@@ -13,7 +14,7 @@ import itertools
 
 def draw_modes(G):
 	maxmode = max([d['mode'] for (u,v,d) in  G.edges(data=True)]) + 1
-	edgelists = [ [(u,v) for (u,v,d) in G.edges(data=True) if d['mode'] == mode ]  for mode in range(maxmode)]
+	edgelists = [ [(u,v) for (u,v,d) in G.edges(data=True) if d['mode'] == mode ]  for mode in range(1, maxmode)]
 	colors = iter(plt.cm.rainbow(np.linspace(0,1,len(edgelists) + 1)))
 
 	pos = nx.spring_layout(G)
@@ -21,6 +22,20 @@ def draw_modes(G):
 	for i, edgelist in enumerate(edgelists):
 		nx.draw_networkx_edges(G, pos, edgelist = edgelist, edge_color = [next(colors)] * len(edgelist))
 
+def lin_syst(G):
+	""" 
+		Return matrices A,B such that
+		[w_1(t+1); w_2(t+1); ...] = A [w_1(t); w_2(t); ...] + B [r_1(t); r_2(t); ...]
+		for modes 1, 2 ...
+	"""
+	maxmode = max([d['mode'] for (u,v,d) in  G.edges(data=True)]) + 1
+	T = nx.adjacency_matrix(G, weight='mode')
+	T_list = [ T == mode for mode in range(1, maxmode) ]
+
+	A = scipy.sparse.block_diag(tuple(T_list), dtype=np.int8)	
+	B = scipy.sparse.bmat([ [Ti for i in range(len(T_list))] for Ti in T_list ]) - 2*A
+
+	return A,B
 
 def midx_to_idx(midx, mN):
 	# Given a len(mN)-dimensional matrix of with sides mN stored as a list,
@@ -51,12 +66,12 @@ class Abstraction(object):
 		self.eta = eta
 
 		self.lb = np.array(lb, dtype=np.float64)
-		self.lb -= (self.lb + eta/2) % eta  # make sure that zero is in middle of cell
+		self.lb -= (self.lb + float(eta)/2) % eta  # make sure that zero is in middle of cell
 		self.ub = np.array(ub, dtype=np.float64)
 		self.ub += (self.ub - self.lb) % eta # make domain number of eta's
-		self.nextMode = 0
+		self.nextMode = 1
 
-		# 
+		# number of discrete points along each dimension
 		self.n_dim = np.round((self.ub - self.lb)/eta).astype(np.uint64)
 
 		# Create a graph and populate it with nodes
@@ -64,7 +79,7 @@ class Abstraction(object):
 		for midx in itertools.product(*[range(dim) for dim in self.n_dim]):
 			cell_lb = self.lb + np.array(midx) * eta
 			cell_ub = cell_lb + eta
-			self.graph.add_node(midx, lb = cell_lb, ub = cell_ub, mid = (cell_lb + cell_ub) / 2)
+			self.graph.add_node(midx, lb = tuple(cell_lb), ub = tuple(cell_ub), mid = (cell_lb + cell_ub) / 2)
 
 	def get_midx_pt(self, pt):
 		""" 
@@ -100,12 +115,12 @@ class Abstraction(object):
 	def add_mode(self, vf, tau):
 		dummy_vf = lambda z,t : vf(z)
 		for node, attr in self.graph.nodes_iter(data=True):
-			x_fin = integrate.odeint(dummy_vf, attr['mid'], np.arange(0, tau, tau/100))
+			x_fin = scipy.integrate.odeint(dummy_vf, attr['mid'], np.arange(0, tau, tau/100))
 			if self.contains(x_fin[-1]):
 				midx = self.get_midx_pt(x_fin[-1])
 				self.graph.add_edge( node, midx, mode = self.nextMode )
 			else:
-				print "Warning: there are transitions out of domain"
+				print "Warning: transition from ", attr['mid'], " in mode ", self.nextMode, " goes out of domain"
 		self.nextMode += 1
 
 def verify_bisim(data):
@@ -141,6 +156,7 @@ def tree_abstr(data):
 
 def example_abstr():
 	data = {}
+
 	# Define a vector fields
 	data['vf'] = [lambda x : [-(x[0]-1) + x[1], -(x[0]-1) - x[1]], 
 	              lambda x : [-(x[0]+1) + x[1], -(x[0]+1) - x[1]]]
@@ -167,8 +183,8 @@ def example_abstr():
 
 def main():
 	ab = example_abstr()
-	# plot_treeineq(data)
-	# ab.plot_planar()	
+
+	return 1
 
 if __name__ == '__main__':
 	main()
--- treeabstr_test.py
+++ treeabstr_test.py
@@ -1,8 +1,10 @@
 import unittest
 
 import numpy as np
+import networkx as nx
+import scipy.linalg
 
-from treeabstr import midx_to_idx, idx_to_midx, Abstraction
+from treeabstr import midx_to_idx, idx_to_midx, Abstraction, lin_syst
 
 class treeabstrTests(unittest.TestCase):
 
@@ -19,15 +21,28 @@ class treeabstrTests(unittest.TestCase):
 
 	def test_abstraction(self):
 		ab = Abstraction([-1, -1], [1, 1], 1)
-		self.assertEquals(len(ab.cell_list), 4)
+		self.assertEquals(len(ab.graph), 9)
 
-		c1 = ab.get_cell(ab.get_midx_pt((0.5, 0.5)))
-		self.assertEquals(tuple(c1.lb), (0,0))
-		self.assertEquals(tuple(c1.ub), (1,1))
+		c1 = ab.get_midx_pt((0.2, 0.2))
+		self.assertEquals(ab.graph.node[c1]['lb'], (-0.5,-0.5))
+		self.assertEquals(ab.graph.node[c1]['ub'], (0.5,0.5))
 
-		c2 = ab.get_cell(ab.get_midx_pt((0.5, -0.3)))
-		self.assertEquals(tuple(c2.lb), (0,-1))
-		self.assertEquals(tuple(c2.ub), (1,0))
+		c2 = ab.get_midx_pt((0.2, -0.7))
+		self.assertEquals(ab.graph.node[c2]['lb'], (-0.5,-1.5))
+		self.assertEquals(ab.graph.node[c2]['ub'], (0.5,-0.5))
+
+	def test_lin_syst(self):
+		g = nx.DiGraph()
+		g.add_nodes_from([1,2,3])
+		g.add_path([2,1], mode=1)
+		g.add_path([3,1], mode=1)
+		g.add_path([1,2,3], mode=2)
+		A,B = lin_syst(g)
+
+		T = np.array([[0, 0, 0], [1, 0, 0], [1, 0, 0]])
+		Tbar = np.array([[0, 1, 0], [0, 0, 1], [0, 0, 0]])
+		self.assertTrue(np.all(A.toarray() == scipy.linalg.block_diag(T, Tbar)))
+		self.assertTrue(np.all(B.toarray() == np.vstack([np.hstack([-T, T]), np.hstack([Tbar, -Tbar])])  ))
 
 if __name__ == '__main__':
 	unittest.main()
